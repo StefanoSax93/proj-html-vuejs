b883863b1f4954cdcd79c4027a9c35aa4893a5ea
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-07-01 11:44:56 +0200

    aggiunta componenti principali + favicon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    
+    <TheHeader/>
+    <TheMain/>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
-
+import TheHeader from './components/TheHeader.vue'
+import TheMain from './components/TheMain.vue'
+import TheFooter from './components/TheFooter.vue'
 
 export default {
   name: 'App',
-  components: {
-    
-  }
+  components: {TheHeader,TheMain,TheFooter}
 }
 </script>
 
---
+++ src/components/TheMain.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheMain',
+        components: {
+
+        }
+    }
+
+</script>
+
+<style lang="scss">
+
+</style>
